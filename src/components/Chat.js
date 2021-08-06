32595555d35cffc4e9b77e93ad89af54c063a11f
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-08-06 19:22:28 -0400

    message styling finished

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,60 +3,16 @@ import "./Chat.css";
 import { Avatar } from "@material-ui/core";
 import AttachFileTwoToneIcon from "@material-ui/icons/AttachFileTwoTone";
 import SendIcon from "@material-ui/icons/Send";
+import { firebase } from "../services/firebase";
 
 function Chat(props) {
-  const fakeMessages = [
-    { id: "1254", content: "Hello", senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2" },
-    { id: "1255", content: "hi", senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973" },
-    {
-      id: "12155",
-      content: "How are you",
-      senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
-    },
-    { id: "1256", content: "Good", senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973" },
-    {
-      id: "1257",
-      content: "How about you?",
-      senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
-    },
-    {
-      id: "1258",
-      content: "Great!!, happy for you",
-      senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
-    },
-    {
-      id: "124545",
-      content: "Let us catch up tmrw?",
-      senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
-    },
-    { id: "1212", content: "Sure!", senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2" },
-    {
-      id: "1241",
-      content: "5PM NYC drive?",
-      senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
-    },
-    {
-      id: "1242",
-      content: "Sounds good",
-      senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
-    },
-    {
-      id: "12543",
-      content: "Will reach your home by 2PM",
-      senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
-    },
-    {
-      id: "1244",
-      content: "Cool!! Excited",
-      senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
-    },
-  ];
   // console.log(props);
-  const { id: personUID, username } = props.person;
+  const { id: personUID, username: personName } = props.person;
   const { currentUID } = props;
+  // console.log("Displaying chat history of ", personUID);
   const messagesEndRef = useRef(null);
   const [message, setMessage] = useState();
-  const [messages, setMessages] = useState(fakeMessages);
+  const [messages, setMessages] = useState([]);
 
   const postMessage = (event) => {
     event.preventDefault();
@@ -74,20 +30,82 @@ function Chat(props) {
     const scrollToBottom = () => {
       messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
-    // console.log("Invoked");
     scrollToBottom();
     setMessages(messages);
   }, [messages]);
+  useEffect(() => {
+    const fakeMessages = [
+      {
+        id: "1254",
+        content: "Hello",
+        senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
+      },
+      { id: "1255", content: "hi", senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973" },
+      {
+        id: "12155",
+        content: "How are you",
+        senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
+      },
+      {
+        id: "1256",
+        content: "Good",
+        senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
+      },
+      {
+        id: "1257",
+        content: "How about you?",
+        senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
+      },
+      {
+        id: "1258",
+        content: "Great!!, happy for you",
+        senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
+      },
+      {
+        id: "124545",
+        content: "Let us catch up tmrw?",
+        senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
+      },
+      {
+        id: "1212",
+        content: "Sure!",
+        senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
+      },
+      {
+        id: "1241",
+        content: "5PM NYC drive?",
+        senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
+      },
+      {
+        id: "1242",
+        content: "Sounds good",
+        senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
+      },
+      {
+        id: "12543",
+        content: "Will reach your home by 2PM",
+        senderID: "dAmZ6pW8CXPy4Dff2zJDxUqMy973",
+      },
+      {
+        id: "1244",
+        content: "Cool!! Excited",
+        senderID: "jOJpmz6s0xVz86npV8KrmJnYnFy2",
+      },
+    ];
+    console.log("Fetched records");
+    // Fetch chat history
+    setMessages(fakeMessages);
+  }, [personName]);
 
   return (
     <div className="chat">
       <header className="chat__header">
         <Avatar
           className="chat__header__avatar"
-          alt={username}
-          src={`https://avatars.dicebear.com/api/initials/${username}.svg?background=%230000ff`}
+          alt={personName}
+          src={`https://avatars.dicebear.com/api/initials/${personName}.svg?background=%230000ff`}
         />
-        <h5>{username}</h5>
+        <h5>{personName}</h5>
       </header>
       {/* Header with avatar & name */}
       <div className="chat__ground">
@@ -101,13 +119,13 @@ function Chat(props) {
               }
             >
               {message.content}
+              <span className="chat__message__timestamp">Time</span>
             </p>
           </div>
         ))}
         {/* Responsible for showing the recent most message */}
         <div ref={messagesEndRef} />
       </div>
-
       {/* Chat history block */}
       <div className="footer">
         <footer>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -11,4 +11,32 @@ const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
+// async function test() {
+//   const userID = "jOJpmz6s0xVz86npV8KrmJnYnFy2";
+//   let snapshot = firebase
+//     .firestore()
+//     .collection("users")
+//     .doc(userID)
+//     .collection("chats");
+//   let data = await snapshot.get();
+//   console.log("User:", userID);
+//   console.log("Chats with the following people:");
+//   data.forEach((doc) => {
+//     console.log(doc.data());
+//   });
+//   snapshot = await snapshot
+//     .doc("dAmZ6pW8CXPy4Dff2zJDxUqMy973")
+//     .collection("messages");
+//   data = await snapshot.get();
+//   data.forEach((doc) => {
+//     const time = doc.data().timestamp.toDate();
+//     const short = time.toLocaleTimeString("en-US", {
+//       hour: "2-digit",
+//       minute: "2-digit",
+//     });
+
+//     console.log(short);
+//   });
+// }
+// test();
 export { db, auth, storage };
