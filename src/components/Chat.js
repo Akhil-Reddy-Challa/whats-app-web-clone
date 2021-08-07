8e6872e26eba1af82e48bc42d7a54f7bae921784
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-08-07 19:24:26 -0400

    emoji dashboard integration complete

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Chat.css";
 import { Avatar, IconButton, Tooltip } from "@material-ui/core";
-import { Send, Search, AttachFileTwoTone, MoreVert } from "@material-ui/icons";
+import {
+  Send,
+  Search,
+  AttachFileTwoTone,
+  MoreVert,
+  EmojiEmotionsOutlined,
+} from "@material-ui/icons";
 import { db } from "../services/firebase";
 import firebase from "firebase";
+import Picker from "emoji-picker-react";
 
 function Chat(props) {
   const { currentUserID } = props;
@@ -11,7 +18,11 @@ function Chat(props) {
   const messagesEndRef = useRef(null);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [emojiBoard, setEmojiBoard] = useState(false);
 
+  const onEmojiClick = (event, emojiObject) => {
+    setMessage(message.concat(emojiObject.emoji));
+  };
   const postMessage = (event) => {
     event.preventDefault();
     // console.log("Postinggg...", message);
@@ -115,17 +126,32 @@ function Chat(props) {
             </p>
           </div>
         ))}
+        {emojiBoard && (
+          <div className="emojiBoard">
+            <Picker onEmojiClick={onEmojiClick} />
+          </div>
+        )}
         {/* Responsible for showing the recent most message */}
         <div ref={messagesEndRef} />
       </div>
       {/* Chat history block */}
+
       <div className="footer">
         <footer>
           <form className="chat__footer" onSubmit={(e) => postMessage(e)}>
-            <AttachFileTwoTone
-              className="chat__footer__fileupload"
-              fontSize="medium"
-            />
+            <Tooltip title="Emoji Icon Picker">
+              <IconButton
+                aria-label="emojiPicker"
+                onClick={() => setEmojiBoard(!emojiBoard)}
+              >
+                <EmojiEmotionsOutlined />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="File Picker">
+              <IconButton aria-label="filePicker">
+                <AttachFileTwoTone className="chat__footer__fileupload" />
+              </IconButton>
+            </Tooltip>
             <input
               type="text"
               className="chat__footer__messageInput"
@@ -136,11 +162,9 @@ function Chat(props) {
               }}
             />
             {message && (
-              <Send
-                fontSize="medium"
-                type="submit"
-                className="chat__footer__messageSend"
-              />
+              <IconButton aria-label="storyIcon">
+                <Send type="submit" className="chat__footer__messageSend" />
+              </IconButton>
             )}
           </form>
         </footer>
