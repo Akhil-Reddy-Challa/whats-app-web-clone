c0bd7231cffda3f62933d8f5ec8b8bbee4833a31
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-08-08 17:05:40 -0400

    made login page styling responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import "./Login.css";
 import { useHistory } from "react-router-dom";
 import { auth } from "../services/firebase";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(1),
-      width: 500,
-    },
-  },
-}));
 function Login() {
-  const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
@@ -45,30 +34,29 @@ function Login() {
     sessionStorage.setItem("email", email);
   };
   return (
-    <form className={classes.root}>
-      <div>
-        <TextField
-          label="Email"
-          value={email}
-          autoFocus
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <TextField
-          label="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+    <form>
+      <TextField
+        label="Email"
+        value={email}
+        autoFocus
+        fullWidth
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        fullWidth
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <Button
-        variant="contained"
         className="login__submit__button"
-        color="primary"
+        variant="outlined"
         disabled={!email || !password}
         onClick={(e) => handleLogin(e)}
         type="submit"
+        style={{ marginTop: "10px", color: "#01bfa5" }}
+        fullWidth
       >
         Sign In
       </Button>
