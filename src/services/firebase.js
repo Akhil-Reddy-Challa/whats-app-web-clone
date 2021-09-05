a0ef1c2b9944a8a3ba839cd1fd561ad0667a2506
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-09-05 10:03:25 -0400

    .

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase";
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -10,5 +11,4 @@ const firebaseApp = firebase.initializeApp({
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
-
 export { db, auth, storage };
