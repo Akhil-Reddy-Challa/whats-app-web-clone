f286de31f5323a8f3882689a55f87664c62bb269
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-09-09 09:28:55 -0400

    modified message box styling to match whatsapp

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -153,11 +153,30 @@ function Chat(props) {
         )}
         {messages.map((message) => (
           <div key={message.msgID} className="chat__message__body">
+            {message.senderID !== currentUserEmail && (
+              <span
+                className="sender__message__tail"
+                data-testid="tail-in"
+                data-icon="tail-in"
+              >
+                <svg viewBox="0 0 8 13" width="8" height="13">
+                  <path
+                    opacity=".13"
+                    fill="#0000000"
+                    d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"
+                  ></path>
+                  <path
+                    fill="currentColor"
+                    d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"
+                  ></path>
+                </svg>
+              </span>
+            )}
             <div
               className={"chat__message ".concat(
                 message.senderID === currentUserEmail
-                  ? "sender__message"
-                  : "currentuser__message"
+                  ? "currentuser__message"
+                  : "sender__message"
               )}
             >
               {message.content}
@@ -165,6 +184,24 @@ function Chat(props) {
                 {message.timestamp}
               </span>
             </div>
+            {message.senderID === currentUserEmail && (
+              <span
+                data-testid="tail-out"
+                data-icon="tail-out"
+                className="currentuser__message__tail"
+              >
+                <svg viewBox="0 0 8 13" width="8" height="13">
+                  <path
+                    opacity=".13"
+                    d="M5.188 1H0v11.193l6.467-8.625C7.526 2.156 6.958 1 5.188 1z"
+                  ></path>
+                  <path
+                    fill="currentColor"
+                    d="M5.188 0H0v11.193l6.467-8.625C7.526 1.156 6.958 0 5.188 0z"
+                  ></path>
+                </svg>
+              </span>
+            )}
           </div>
         ))}
         {emojiBoard && (
